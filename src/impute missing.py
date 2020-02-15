dd74ccbed2bba2e155481ce437bbb3cb39a1eafa
Naiwen Xu <xnw1127@bu.edu>
2020-02-15 03:14:12 -0500

    update

--- src/impute missing.py
+++ src/impute missing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def import_data(filename):
     with open(filename, "r") as f:
@@ -6,7 +7,7 @@ def import_data(filename):
         X = [[] for _ in range(len(lines))]
         y = [[] for _ in range(len(lines))]
         
-        for i in range(0, len(lines)):
+        for i in range(0, 2):
             replace = lines[i].replace("?", str(np.nan))
             line = replace.strip().split(",")
             
@@ -19,5 +20,22 @@ def import_data(filename):
             y[i].append(eval(line[279]))
 
     return X,y
+
+def impute_missing(X):
+    for ls in X[0:2]:
+        n = len(ls)
+        sorted_ls = sorted(ls, key=lambda x: x if not math.isnan(x) else 0)
+        
+        if n % 2 == 0:
+            median1 = sorted_ls[n//2]
+            median2 = sorted_ls[n//2 - 1]
+            median = (median1 + median2) / 2
+        else:
+            median = sorted_ls[n//2]
+
+        for val in ls:
+            if math.isnan(val):
+                ls[ls.index(val)] = median
             
-            
+    return X
+        
